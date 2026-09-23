4c0d6a7623cbe767beccd939180ae78407b7a9dd
Dilanka Dewmi <dildewkee@gmail.com>
2026-09-23 21:17:14 +0530

    Implement REST API and web frontend

---
+++ backend/SunriseDentalClinicBackend/src/main/java/com/sunrisedentalclinic/controller/TreatmentController.java
@@ -0,0 +1,4 @@
+package com.sunrisedentalclinic.controller;
+
+public class TreatmentController {
+}
---
+++ backend/SunriseDentalClinicBackend/src/main/java/com/sunrisedentalclinic/util/PasswordHashGenerator.java
@@ -0,0 +1,4 @@
+package com.sunrisedentalclinic.util;
+
+public class PasswordHashGenerator {
+}
